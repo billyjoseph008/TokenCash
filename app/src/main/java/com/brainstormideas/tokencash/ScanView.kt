dd823e81f8d9bcae85a6a59dc36ff2bc0ca7635d
billyjoseph008 <billyjoseph08@hotmail.com>
2020-07-30 09:17:30 -0500

    Vamos con todo!

--- app/src/main/java/com/brainstormideas/tokencash/ScanView.kt
+++ app/src/main/java/com/brainstormideas/tokencash/ScanView.kt
@@ -1,11 +1,141 @@
 package com.brainstormideas.tokencash
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
+import android.webkit.URLUtil
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import java.io.IOException
 
 class ScanView : AppCompatActivity() {
+
+    private var cameraSource: CameraSource? = null
+    private var cameraView: SurfaceView? = null
+    private val MY_PERMISSIONS_REQUEST_CAMERA = 1
+    private var token = ""
+    private var tokenanterior = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan_view)
+        setContentView(R.layout.activity_main)
+
+        cameraView = findViewById<SurfaceView>(R.id.scan_view)
+        initQR()
+    }
+
+    fun initQR() {
+
+        // creo el detector qr
+        val barcodeDetector = BarcodeDetector.Builder(this)
+            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .build()
+
+        // creo la camara
+        cameraSource = CameraSource.Builder(this, barcodeDetector)
+            .setRequestedPreviewSize(1600, 1024)
+            .setAutoFocusEnabled(true) //you should add this feature
+            .build()
+
+        // listener de ciclo de vida de la camara
+        cameraView!!.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+
+                // verifico si el usuario dio los permisos para la camara
+                if (ActivityCompat.checkSelfPermission(this@ScanView, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        // verificamos la version de ANdroid que sea al menos la M para mostrar
+                        // el dialog de la solicitud de la camara
+                        if (shouldShowRequestPermissionRationale(
+                                Manifest.permission.CAMERA))
+                        ;
+                        requestPermissions(arrayOf(Manifest.permission.CAMERA),
+                            MY_PERMISSIONS_REQUEST_CAMERA)
+                    }
+                    return
+                } else {
+                    try {
+                        cameraSource!!.start(cameraView!!.holder)
+                    } catch (ie: IOException) {
+                        Log.e("CAMERA SOURCE", ie.message)
+                    }
+
+                }
+            }
+
+            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource!!.stop()
+            }
+        })
+
+        // preparo el detector de QR
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode>() {
+            override fun release() {}
+
+
+            override fun receiveDetections(detections: Detector.Detections<Barcode>) {
+                val barcodes = detections.getDetectedItems()
+
+                if (barcodes.size() > 0) {
+
+                    // obtenemos el token
+                    token = barcodes.valueAt(0).displayValue.toString()
+
+                    // verificamos que el token anterior no se igual al actual
+                    // esto es util para evitar multiples llamadas empleando el mismo token
+                    if (token != tokenanterior) {
+
+                        // guardamos el ultimo token proceado
+                        tokenanterior = token
+                        Log.i("token", token)
+
+                        if (URLUtil.isValidUrl(token)) {
+                            // si es una URL valida abre el navegador
+                            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(token))
+                            startActivity(browserIntent)
+                        } else {
+                            // comparte en otras apps
+                            val shareIntent = Intent()
+                            shareIntent.action = Intent.ACTION_SEND
+                            shareIntent.putExtra(Intent.EXTRA_TEXT, token)
+                            shareIntent.type = "text/plain"
+                            startActivity(shareIntent)
+                        }
+
+                        Thread(object : Runnable {
+                            override fun run() {
+                                try {
+                                    synchronized(this) {
+                                        wait(5000)
+                                        // limpiamos el token
+                                        tokenanterior = ""
+                                    }
+                                } catch (e: InterruptedException) {
+                                    // TODO Auto-generated catch block
+                                    Log.e("Error", "Waiting didnt work!!")
+                                    e.printStackTrace()
+                                }
+
+                            }
+                        }).start()
+
+                    }
+                }
+            }
+        })
     }
 }
