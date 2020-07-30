5f019a511cecdb57aa60c2c50954520719ca0656
billyjoseph008 <billyjoseph08@hotmail.com>
2020-07-30 08:55:28 -0500

    Initial commit

---
+++ app/src/main/java/com/brainstormideas/tokencash/Notifications.kt
@@ -0,0 +1,11 @@
+package com.brainstormideas.tokencash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notifications : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notifications)
+    }
+}
---
+++ app/src/main/java/com/brainstormideas/tokencash/WalletHistory.kt
@@ -0,0 +1,11 @@
+package com.brainstormideas.tokencash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WalletHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallet_history)
+    }
+}
